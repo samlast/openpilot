44487c8f3a6f7ac9eb4433c640a920e51a33347b
samlast <49180171+samlast@users.noreply.github.com>
2022-12-08 22:38:12 -0500

    Update values.py

--- selfdrive/car/subaru/values.py
+++ selfdrive/car/subaru/values.py
@@ -235,30 +235,37 @@ FW_VERSIONS = {
   },
   CAR.LEGACY_PREGLOBAL: {
     (Ecu.esp, 0x7b0, None): [
+      b'\x8b\x97Dx00',
       b'k\x97D\x00',
       b'[\xba\xc4\x03',
       b'{\x97D\x00',
       b'[\x97D\000',
     ],
     (Ecu.eps, 0x746, None): [
+      b'{\xb0\x00\x00',
       b'[\xb0\x00\x01',
       b'K\xb0\x00\x01',
       b'k\xb0\x00\x00',
     ],
     (Ecu.fwdCamera, 0x787, None): [
+      b'\x00\x00df\x1f@ \n',
       b'\x00\x00c\xb7\x1f@\x10\x16',
       b'\x00\x00c\x94\x1f@\x10\x08',
       b'\x00\x00c\xec\x1f@ \x04',
+      b'\000\000c\xec\037@ \004',
     ],
     (Ecu.engine, 0x7e0, None): [
+      b'\xb5\"@p\a',
       b'\xab*@r\a',
       b'\xa0+@p\x07',
       b'\xb4"@0\x07',
       b'\xa0"@q\a',
     ],
     (Ecu.transmission, 0x7e1, None): [
+      b'\xbc\xf2\x00\x81\x00',
       b'\xbe\xf2\x00p\x00',
       b'\xbf\xfb\xc0\x80\x00',
+      b'\xbd\xf2\x00`\x00',
       b'\xbd\xf2\x00`\x00',
       b'\xbf\xf2\000\x80\000',
     ],
